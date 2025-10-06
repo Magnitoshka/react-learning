40fc7eba3765aec01bf1bad9a3c2973af4b806db
Magnitoshka <92590449+Magnitoshka@users.noreply.github.com>
2025-10-06 18:52:16 +0300

    dumb

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCxQ8m4bQTgFR0tQnhyLoOoJdf5pBV8pDc",
+    authDomain: "studying-project-namba-one.firebaseapp.com",
+    projectId: "studying-project-namba-one",
+    storageBucket: "studying-project-namba-one.firebasestorage.app",
+    messagingSenderId: "912399989831",
+    appId: "1:912399989831:web:1ade829d08c30dc1364562"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
